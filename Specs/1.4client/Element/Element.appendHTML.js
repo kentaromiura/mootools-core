8be01241d0c359bcccaf446399d423e9718d9aaa
Cristian Carlesso <kentaromiura@MacBook-di-Cristian.local>
2012-06-21 00:00:43 +0200

    removed fallback tests
    test only the public api, in order to check the fallback you need a
    browser without insertAdjacent

--- Specs/1.4client/Element/Element.appendHTML.js
+++ Specs/1.4client/Element/Element.appendHTML.js
@@ -10,29 +10,6 @@
 describe('Element.appendHTML', function(){
 
 	var check, base, baseFallBack;
-	var inserters = {
-
-		before: function(context, element){
-			var parent = element.parentNode;
-			if (parent) parent.insertBefore(context, element);
-		},
-
-		after: function(context, element){
-			var parent = element.parentNode;
-			if (parent) parent.insertBefore(context, element.nextSibling);
-		},
-
-		bottom: function(context, element){
-			element.appendChild(context);
-		},
-
-		top: function(context, element){
-			element.insertBefore(context, element.firstChild);
-		}
-
-	};
-
-	inserters.inside = inserters.bottom;
 
 	beforeEach(function(){
 		check = new Element('span', {
@@ -45,23 +22,7 @@ describe('Element.appendHTML', function(){
 		check.inject(document.documentElement);
 		base = $(check.getChildren()[0]);
 		baseFallBack = $(check.getChildren()[1]);
-		//ugly, but I must do this in order to check fallback method
-		baseFallBack.appendHTML = function(html, where){
-			var temp = new Element('div', {html: html}),
-				children = temp.childNodes,
-				fragment = temp.firstChild;
 
-			if (!fragment) return this;
-			if (children.length > 1) {
-				fragment = document.createDocumentFragment();
-				for (var i = 0, l = children.length; i < l; i++) {
-					fragment.appendChild(children[i]);
-				}
-			}
-
-			inserters[where || 'bottom'](fragment, this);
-			return this;
-		};
 		base.set('rel', '0');
 		baseFallBack.set('rel', '1');
 	});
